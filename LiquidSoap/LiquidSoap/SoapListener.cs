294bd7e758ef01c37005e77e17f9b433a48bfe57
Evan Spiker <umdoobby@gmail.com>
2021-10-02 01:47:48 -0400

    async c# host

---
+++ LiquidSoap/LiquidSoap/SoapListener.cs
@@ -0,0 +1,22 @@
+﻿using System.Net;
+using System.Text;
+
+namespace LiquidSoap
+{
+    public class SoapListener
+    {
+        private HttpListener _httpListener;
+        private bool _stop;
+
+        public SoapListener(string[] urls)
+        {
+            _httpListener = new HttpListener();
+            foreach (string url in urls)
+            {
+                _httpListener.Prefixes.Add(url);
+            }
+        }
+        
+        
+    }
+}
